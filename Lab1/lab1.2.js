c62e0d7d0c471848c0d0860fbb91a7698dad98cc
Hieuthutha <dragoncandyx004@gmail.com>
2024-01-06 20:00:18 +0700

    lab1

---
+++ Lab1/lab1.2.js
@@ -0,0 +1,9 @@
+let name1 = "Hung";
+let tuoi1 = 36;
+let name2 = "Tuan";
+let tuoi2 = 27;
+
+let sayHello = function(name, tuoi){
+    console.log(`I'm ${name} I'm ${tuoi}`);
+}
+sayHello(name1, tuoi1);
