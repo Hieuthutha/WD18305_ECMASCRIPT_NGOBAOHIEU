5a48d0467d1fe97a74f4f1a4370ab99f38d9e59c
Hieuthutha <132965970+Hieuthutha@users.noreply.github.com>
2024-01-10 20:21:08 +0700

    Delete hieunbpc06743_Lab1 directory

--- hieunbpc06743_Lab1/lab1.2.js
+++
@@ -1,9 +0,0 @@
-let name1 = "Hung";
-let tuoi1 = 36;
-let name2 = "Tuan";
-let tuoi2 = 27;
-
-let sayHello = function(name, tuoi){
-    console.log(`I'm ${name} I'm ${tuoi}`);
-}
-sayHello(name1, tuoi1);
